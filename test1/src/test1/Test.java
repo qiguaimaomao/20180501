4d5baf7ffd03f7de6dfd10a1deda8e0c17ba602b
jiuyv <jiuyv@qiguai>
2018-09-17 11:02:59 +0800

    change main

--- test1/src/test1/Test.java
+++ test1/src/test1/Test.java
@@ -1,7 +1,13 @@
 package test1;
 
 public class Test {
-public static void main(String[] args) {
+public static void main(String[] args)  {
 	System.out.println("hello world");
+	for (int i = 0; i < 100000000; i++) {
+		System.out.println(i);
+		
+	}
 }
+
+
 }
