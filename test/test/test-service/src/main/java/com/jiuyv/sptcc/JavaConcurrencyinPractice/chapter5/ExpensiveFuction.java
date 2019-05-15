8293b6904164cd57b4e7e18691da369144b933de
jiuyv <jiuyv@qiguai>
2019-05-15 17:19:33 +0800

    实现缓存

---
+++ test/test/test-service/src/main/java/com/jiuyv/sptcc/JavaConcurrencyinPractice/chapter5/ExpensiveFuction.java
@@ -0,0 +1,13 @@
+package com.jiuyv.sptcc.JavaConcurrencyinPractice.chapter5;
+
+import java.math.BigInteger;
+
+public class ExpensiveFuction implements Computable<String,BigInteger>{
+
+	@Override
+	public BigInteger compute(String arg) throws InterruptedException {
+		return new BigInteger(arg);
+	}
+
+
+}
