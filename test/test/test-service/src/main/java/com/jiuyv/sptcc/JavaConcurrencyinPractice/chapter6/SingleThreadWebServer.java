a353068e7890ecbbede56f456208b2b62f44bf0e
jiuyv <jiuyv@qiguai>
2019-05-16 11:12:30 +0800

    增加第六章

---
+++ test/test/test-service/src/main/java/com/jiuyv/sptcc/JavaConcurrencyinPractice/chapter6/SingleThreadWebServer.java
@@ -0,0 +1,24 @@
+package com.jiuyv.sptcc.JavaConcurrencyinPractice.chapter6;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+/**
+ * 顺序化的webserver
+ * @author jiuyv
+ *
+ */
+public class SingleThreadWebServer {
+public static void main(String[] args) throws IOException {
+	ServerSocket server=new ServerSocket(80);
+	while (true) {
+		Socket socket=server.accept();
+		handleRequest(socket);
+	}
+}
+
+private static void handleRequest(Socket socket) {
+	// TODO Auto-generated method stub
+	
+}
+}
