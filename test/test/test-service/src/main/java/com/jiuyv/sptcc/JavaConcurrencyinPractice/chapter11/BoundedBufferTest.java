d2c2bce70d52259955afe835633350183da23323
qiguaimaomao <wangkeee@163.com>
2019-05-29 22:53:22 +0800

    测试阻塞与响应中断

--- test/test/test-service/src/main/java/com/jiuyv/sptcc/JavaConcurrencyinPractice/chapter11/BoundedBufferTest.java
+++ test/test/test-service/src/main/java/com/jiuyv/sptcc/JavaConcurrencyinPractice/chapter11/BoundedBufferTest.java
@@ -1,6 +1,7 @@
 package com.jiuyv.sptcc.JavaConcurrencyinPractice.chapter11;
 
 import junit.framework.TestCase;
+import junit.framework.TestResult;
 
 public class BoundedBufferTest extends TestCase{
 public void testIsEmptyWhenConstructed() {
@@ -16,5 +17,33 @@ public void testIsFullAfterPuts() throws InterruptedException {
 	}
 	assertFalse(bb.isEmpty());
 	assertTrue(bb.isFull());
+}
+
+public void testTakeBlocksWhenEmpty() {
+	final BoundedBuffer<Integer> bb=new BoundedBuffer<>(10);
+Thread taker=new Thread(){
+	@Override
+		public void run() {
+		try {
+			int used=bb.take();
+			fail();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+//			e.printStackTrace();
+		}
+		
+		}
+};
+
+
+try {
+	taker.start();
+	Thread.sleep(1000L);
+	taker.interrupt();
+	taker.join(1000L);
+	assertFalse(taker.isAlive());
+} catch (Exception e) {
+	fail();
+}
 }
 }
